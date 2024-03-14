6e56dd138c8509f2c5c95eac5e668b105c5509ef
Blackmanx <blackman@sajl.tk>
2024-03-15 00:38:23 +0100

    sm8450-common: Switch UI rendering to vulkan
    * Fixes most lags in UI
    * We are going to use oculus quest 2's drivers since they fix most of vulkan bugs
    * Enable HWUI optimization flags as well

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,6 +94,10 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
 
+# HWUI
+TARGET_USES_VULKAN := true
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_oplus
 
