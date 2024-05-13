609288335ba8973ace793c5d5d4885c7348d6cdc
RAM-UNLOK <rakmoparte@gmail.com>
2024-05-14 04:05:21 +0530

    sm8450-common: Update compatibility matrix & Manifest

--- common.mk
+++ common.mk
@@ -297,7 +297,8 @@ PRODUCT_PACKAGES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor
+    android.system.net.netd@1.1.vendor \
+    netutils-wrapper-1.0
 
 # Neural network
 PRODUCT_PACKAGES += \
