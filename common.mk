8687d2eb1f7256afa478c6f180e532312048981d
ram-unlok <rakmoparte@gmail.com>
2024-05-23 01:07:30 +0530

    sm8450-common: Use stock shared lib for.......
    libagm
    vendor.qti.hardware.AGMIPC
    vendor.qti.hardware.pal
    
    Fixes Build Error 
    
    [1mhardware/qcom-caf/sm8450/audio/primary-hal/hal/Android.mk: [31merror:[0m[1m "audio.primary.taro (SHARED_LIBRARIES android-arm64) missing vendor.qti.hardware.AGMIPC@1.0-impl (SHARED_LIBRARIES android-arm64)" [0m
    You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
    [1mhardware/qcom-caf/sm8450/audio/primary-hal/hal/Android.mk: [31merror:[0m[1m "audio.primary.taro (SHARED_LIBRARIES android-arm64) missing libagm (SHARED_LIBRARIES android-arm64)" [0m
    You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
    [1mhardware/qcom-caf/sm8450/audio/primary-hal/hal/Android.mk: [31merror:[0m[1m "audio.primary.taro (SHARED_LIBRARIES android-arm) missing vendor.qti.hardware.AGMIPC@1.0-impl (SHARED_LIBRARIES android-arm)" [0m
    You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
    [1mhardware/qcom-caf/sm8450/audio/primary-hal/hal/Android.mk: [31merror:[0m[1m "audio.primary.taro (SHARED_LIBRARIES android-arm) missing libagm (SHARED_LIBRARIES android-arm)" [0m
    You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.

--- common.mk
+++ common.mk
@@ -60,9 +60,7 @@ PRODUCT_PACKAGES += \
     libsndcardparser \
     libtinycompress \
     libvisualizer \
-    libvolumelistener \
-    vendor.qti.hardware.AGMIPC@1.0-service \
-    vendor.qti.hardware.pal@1.0-impl
+    libvolumelistener
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8450/audio/primary-hal
 QCV_FAMILY_SKUS := cape taro ukee
