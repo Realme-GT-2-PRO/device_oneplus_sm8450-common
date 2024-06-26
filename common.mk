79636fbf04d8d13795e719779d62f3342be409e7
ram-unlok <rakmoparte@gmail.com>
2024-06-26 17:52:40 +0530

    sm8450-common: Add Missing Camera postprocessing libraries
    06-26 16:36:14.635  1232  1232 E ChiOfflinePostproc: PostProcService: No postproc lib, dlopen failed

--- common.mk
+++ common.mk
@@ -110,6 +110,9 @@ PRODUCT_PACKAGES += \
     android.frameworks.cameraservice.service@2.2.vendor \
     android.frameworks.sensorservice@1.0.vendor \
     android.frameworks.stats-V1-ndk_platform.vendor \
+    android.hardware.camera.provider@2.4.vendor \
+    android.hardware.camera.provider@2.5.vendor \
+    android.hardware.camera.provider@2.6.vendor \
     android.hardware.camera.provider@2.7.vendor \
     android.hardware.graphics.common-V4-ndk \
     camera.device@1.0-impl \
