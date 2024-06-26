ff2c147fe201d8dad6e736d0f4d88dbf4cb0cc96
ram-unlok <rakmoparte@gmail.com>
2024-06-27 02:16:53 +0530

    sm8450-common: Register android.hardware.health@2.1 HAL
    06-26 16:36:09.568   718  1993 W libc    : Unable to set property "ctl.interface_start" to "android.hardware.health@2.1::IHealth/default": error code: 0x20
    06-26 16:36:09.568   718  1993 I hwservicemanager: Tried to start android.hardware.health@2.1::IHealth/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.

--- common.mk
+++ common.mk
@@ -252,8 +252,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health-service.qti \
+    android.hardware.health@2.1.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
