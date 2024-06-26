c5b91635cf9dd17ae8463f91a5fdcc53b4064291
ram-unlok <rakmoparte@gmail.com>
2024-06-26 18:52:07 +0530

    sm8450-common: Add Missing BT library

--- common.mk
+++ common.mk
@@ -95,6 +95,15 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Bluetooth Library Deps
+PRODUCT_PACKAGES += \
+    liblhdc \
+    liblhdcv5 \
+    libldacBT_abr \
+    libldacBT_enc \
+    liblhdcBT_enc \
+    liblhdcv5BT_enc
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
